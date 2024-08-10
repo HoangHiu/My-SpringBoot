4a28c33be714b8500268c0c50af8959330e18040
Hoang Hieu <hoangdinhhieu2105.work@gmail.com>
2024-08-10 11:45:00 +0700

    SpringBoot-Hello-World

---
+++ springHelloWorld/src/test/java/com/myapp/springHelloWorld/SpringHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.springHelloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
